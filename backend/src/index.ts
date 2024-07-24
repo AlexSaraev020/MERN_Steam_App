7a8d629d80d42f9659b35c868554ee9d696f383e
Alex Saraev <114063388+AlexSaraev020@users.noreply.github.com>
2024-07-24 20:13:46 +0300

    Deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://gamerslobby.vercel.app",
+  origin: "https://gamerslobby.onrender.com",
   methods: ["GET", "POST" , "DELETE" , "PUT"],
   credentials: true,
 }));
